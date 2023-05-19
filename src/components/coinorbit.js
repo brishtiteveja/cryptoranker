7289592e6e6b531bd0bcbf20a3ef7c59c41e63e6
Andy Khan (Ananda) <brishtiteveja@gmail.com>
2023-05-19 16:09:55 -0500

    Modal popup when clicking on the planet showing the info.

--- src/components/coinorbit.js
+++ src/components/coinorbit.js
@@ -1,8 +1,11 @@
-import { useEffect } from "react";
+import * as THREE from "three";
+import { useEffect, useRef } from "react";
 import SceneInit from "../lib/SceneInit";
 import Planet from "../lib/Planet";
 
 const CoinOrbit = ({ data }) => {
+  const sceneRef = useRef(null);
+
   useEffect(() => {
     async function sceneManage() {
       let sceneManager = new SceneInit();
@@ -21,12 +24,18 @@ const CoinOrbit = ({ data }) => {
         for (let i = 0; i < count; i++) {
           const radius = 1;
           const orbitRadius = 10 + i * 5;
-          const orbitSpeed = Math.random() * 0.005;
+          const orbitSpeed = Math.random() * 0.0005;
           const planetSize = 0.2 + Math.random() * 0.8;
           const color = randomColor();
-          const textureFile = "bitcoin.jpg"
+          const textureFile = "bitcoin.jpg";
+          const planetInfo = {
+            name: `Planet ${i + 1}`,
+            mass: `${Math.random() * 1000} kg`,
+            distance: `${orbitRadius} AU`,
+            orbitalPeriod: `${Math.random() * 100} years`
+          };
 
-          data.push([radius, orbitRadius, orbitSpeed, planetSize, color, textureFile]);
+          data.push([radius, orbitRadius, orbitSpeed, planetSize, color, textureFile, planetInfo]);
         }
 
         return data;
@@ -34,27 +43,52 @@ const CoinOrbit = ({ data }) => {
 
       const planetData = generatePlanetData(25);
 
-      // const solarSystem = new THREE.Group()
-      let i = 0;
-      planetData.forEach(([radius, orbitRadius, orbitSpeed, planetSize, color, textureFile]) => {
-        const planet = new Planet(radius, orbitRadius, orbitSpeed, planetSize, color, textureFile)
+      const planets = planetData.map(([radius, orbitRadius, orbitSpeed, planetSize, color, textureFile, planetInfo], index) => {
+        const planet = new Planet(radius, orbitRadius, orbitSpeed, planetSize, color, textureFile, planetInfo);
         const planetMesh = planet.getMesh();
 
+        planetMesh.name = `planet-${index}`;
+
         sceneManager.scene.add(planetMesh);
-        planet.drawOrbit(sceneManager.scene, randomColor(), i + 1);
-        i += 1;;
-        sceneManager.planets.push(planet);
+
+        planet.drawOrbit(sceneManager.scene, color, index + 1);
+
+        return planet;
       });
+
+      sceneManager.planets = planets;
+
+      const handlePlanetClick = (event) => {
+        const canvasBounds = sceneRef.current.getBoundingClientRect();
+        const x = event.clientX - canvasBounds.left;
+        const y = event.clientY - canvasBounds.top;
+        const mouse = new THREE.Vector2((x / canvasBounds.width) * 2 - 1, -(y / canvasBounds.height) * 2 + 1);
+
+        const raycaster = new THREE.Raycaster();
+        raycaster.setFromCamera(mouse, sceneManager.camera);
+
+        const intersects = raycaster.intersectObjects(sceneManager.scene.children, true);
+
+        for (const intersect of intersects) {
+          if (intersect.object.name.startsWith("planet-")) {
+            const planetIndex = parseInt(intersect.object.name.split("-")[1]);
+            const planet = planets[planetIndex];
+            planet.displayPlanetInfo();
+          }
+        }
+      };
+
+      sceneManager.renderer.domElement.addEventListener("click", handlePlanetClick);
     }
-    
+
     sceneManage();
   }, []);
 
   return (
     <div className="flex flex-col items-center justify-center">
-      <canvas id="myThreeJsCanvas" className="ml-20" />
+      <canvas id="myThreeJsCanvas" className="ml-20" ref={sceneRef} />
     </div>
   );
-}
+};
 
 export default CoinOrbit;
--- src/lib/Planet.js
+++ src/lib/Planet.js
@@ -3,7 +3,7 @@ import { FontLoader } from 'three/examples/jsm/loaders/FontLoader.js';
 import { TextGeometry } from 'three/examples/jsm/geometries/TextGeometry.js';
 
 export default class Planet {
-  constructor(radius, orbitRadius, orbitSpeed, planetSize, color, textureFile) {
+  constructor(radius, orbitRadius, orbitSpeed, planetSize, color, textureFile, planetInfo) {
     this.radius = radius;
     this.orbitRadius = orbitRadius;
     this.orbitAngle = Math.random() * 2 * Math.PI;
@@ -11,19 +11,28 @@ export default class Planet {
     this.planetSize = planetSize;
     this.color = color;
     this.textureFile = textureFile;
+    this.planetInfo = planetInfo;
+
+    this.mesh = null;
+    this.orbitLine = null;
+    this.textMesh = null;
   }
 
   getMesh() {
-    if (this.mesh === undefined || this.mesh === null) {
+    if (this.mesh === null) {
       const geometry = new THREE.SphereGeometry(this.radius);
-      this.mesh = new THREE.Mesh(geometry);
-      
+      const material = new THREE.MeshBasicMaterial({ color: this.color });
+      this.mesh = new THREE.Mesh(geometry, material);
+
       this.mesh.position.set(
         this.orbitRadius * Math.cos(this.orbitAngle),
         this.orbitRadius * Math.sin(this.orbitAngle),
         0
       );
+
+      this.mesh.onClick = this.displayPlanetInfo.bind(this);
     }
+
     return this.mesh;
   }
 
@@ -35,52 +44,6 @@ export default class Planet {
       0
     );
   }
-
-  // drawOrbit(scene, color, number) {
-  //   const orbitGeometry = new THREE.BufferGeometry();
-  //   const points = [];
-  //   for (let i = 0; i < 100; i++) {
-  //     const radian = (i / 100) * Math.PI * 2;
-  //     points.push(
-  //       new THREE.Vector3(
-  //         Math.cos(radian) * this.orbitRadius,
-  //         Math.sin(radian) * this.orbitRadius,
-  //         0
-  //       )
-  //     );
-  //   }
-  //   orbitGeometry.setFromPoints(points);
-  //   const orbitMaterial = new THREE.LineBasicMaterial({ color: new THREE.Color(color) });
-  //   const orbitLine = new THREE.LineLoop(orbitGeometry, orbitMaterial);
-  //   scene.add(orbitLine);
-
-  //   // Create font loader
-  //   const loader = new FontLoader();
-  //   loader.load( 'fonts/helvetiker_regular.typeface.json', function ( font ) {
-  //     const textMeshSize = 10;
-  //     const textHeight = 0;
-  //     const textGeo = new TextGeometry( number.toString(), {
-  //       font: font,
-  //       size: textMeshSize,
-  //       height: textHeight,
-  //       curveSegments: 12,
-  //       bevelEnabled: true,
-  //       bevelThickness: 10,
-  //       bevelSize: 8,
-  //       bevelOffset: 0,
-  //       bevelSegments: 5
-  //     } );
-
-  //     const textMaterial = new THREE.MeshPhongMaterial( { color: 0xff0000 } );
-  //     const mesh = new THREE.Mesh( textGeo, textMaterial );
-  //     mesh.position.set(
-  //       (this.orbitRadius + 100) * Math.cos(this.orbitAngle),
-  //       (this.orbitRadius + 100) * Math.sin(this.orbitAngle),
-  //       0
-  //     );
-  //     scene.add( mesh );
-  //   }.bind(this) );
-  // }
 
   drawOrbit(scene, color, number) {
     const orbitGeometry = new THREE.BufferGeometry();
@@ -97,9 +60,9 @@ export default class Planet {
     }
     orbitGeometry.setFromPoints(points);
     const orbitMaterial = new THREE.LineBasicMaterial({ color: new THREE.Color(color) });
-    const orbitLine = new THREE.LineLoop(orbitGeometry, orbitMaterial);
-    scene.add(orbitLine);
-  
+    this.orbitLine = new THREE.LineLoop(orbitGeometry, orbitMaterial);
+    scene.add(this.orbitLine);
+
     const loader = new FontLoader();
     loader.load('fonts/helvetiker_regular.typeface.json', (font) => {
       const textMeshSize = 1; // Adjust the size of the text mesh
@@ -108,26 +71,60 @@ export default class Planet {
         font: font,
         size: textMeshSize,
         height: textHeight,
-        // curveSegments: 12,
-        // bevelEnabled: true,
-        // bevelThickness: 10,
-        // bevelSize: 8,
-        // bevelOffset: 0,
-        // bevelSegments: 5
       });
-  
+
       const textMaterial = new THREE.MeshBasicMaterial({ color: new THREE.Color(color) });
-      const textMesh = new THREE.Mesh(textGeo, textMaterial);
-  
+      this.textMesh = new THREE.Mesh(textGeo, textMaterial);
+
       // Calculate the position of the text mesh on the orbit
       const textPosition = new THREE.Vector3(
         this.orbitRadius + textMeshSize * 2, // Adjust the offset based on the text size
         0,
         0
       );
-  
-      textMesh.position.copy(textPosition);
-      scene.add(textMesh);
+
+      this.textMesh.position.copy(textPosition);
+      scene.add(this.textMesh);
+
+      this.textMesh.onClick = this.displayPlanetInfo.bind(this);
     });
   }
+
+  displayPlanetInfo() {
+    const modal = document.createElement('div');
+    modal.classList.add('fixed', 'inset-0', 'flex', 'items-center', 'justify-center', 'z-50');
+  
+    const modalContent = document.createElement('div');
+    modalContent.classList.add('bg-blue-200', 'bg-opacity-75', 'rounded', 'shadow-lg', 'p-4', 'text-white');
+  
+    const title = document.createElement('h2');
+    title.classList.add('text-2xl', 'font-bold', 'mb-2');
+    title.textContent = this.planetInfo.name;
+  
+    const mass = document.createElement('p');
+    mass.classList.add('text-lg', 'font-bold', 'mb-2', 'text-blue-500');
+    mass.textContent = `Mass: ${this.planetInfo.mass}`;
+  
+    const distance = document.createElement('p');
+    distance.classList.add('text-lg', 'font-bold', 'mb-2', 'text-green-500');
+    distance.textContent = `Distance from Sun: ${this.planetInfo.distance}`;
+  
+    const orbitalPeriod = document.createElement('p');
+    orbitalPeriod.classList.add('text-lg', 'font-bold', 'mb-2', 'text-yellow-500');
+    orbitalPeriod.textContent = `Orbital Period: ${this.planetInfo.orbitalPeriod}`;
+  
+    modalContent.appendChild(title);
+    modalContent.appendChild(mass);
+    modalContent.appendChild(distance);
+    modalContent.appendChild(orbitalPeriod);
+  
+    modal.appendChild(modalContent);
+    document.body.appendChild(modal);
+  
+    modal.addEventListener('click', () => {
+      modal.remove();
+    });
+  }
+  
+   
 }
